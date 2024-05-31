f7630c1281c3c2f6fd02cc771a3f974b73e6fc6b
Ahnaf Ahmad <72279203+ahnafahmad0425@users.noreply.github.com>
2024-05-30 19:38:15 -0500

    "initial upload"

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ahnaf Ahmad on 5/30/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    
+    let rate : Double
+    
+}
---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Ahnaf Ahmad on 5/30/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+    let currency : String
+    let price : String
+}
